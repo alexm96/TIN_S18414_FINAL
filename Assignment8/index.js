6a285e52168f33776413ac6cdf0a47f2310bca89
Alex Martynek <s18414@pjwstk.edu.pl>
2020-11-10 19:01:38 +0100

    Fixed janky code for state, changed to query param

--- Assignment8/index.js
+++ Assignment8/index.js
@@ -2,6 +2,7 @@ const Student=require('./models/student')
 const express = require('express')
 const bodyparser=require('body-parser')
 const path = require('path');
+const { nextTick } = require('process');
 const app = express()
 app.use(bodyparser.urlencoded({extended:false}))
 app.use(express.static(__dirname + '/public'));
@@ -9,22 +10,24 @@ app.set('view engine', 'ejs');
 const port =3000
 const savedUsers=[]
 
+
 app.get("/hello",(req,res)=>{
     res.send("Hello world")
 })
+const formControl=(req,res)=>{
+    
+    const successVal=req.query.success    
+    res.render((path.join(__dirname+'/templates/form.ejs')),{success:successVal})
+}
 
-app.get("/form",(req,res)=>{
-    res.sendFile((path.join(__dirname+'/form.html')))
-})
+
 
 app.post("/formdata",(req,res)=>{
-    console.log(req.body)
-    
     const newStudent=new Student(req.body.fName,req.body.lName,req.body.sNumber)
-    console.log(newStudent)
     savedUsers.push(newStudent);
-    console.log(savedUsers);
-    res.redirect("/formdata")
+    
+    res.redirect("/form?success=true")
+    
 })
 
 app.get("/formdata",(req,res)=>{
@@ -32,7 +35,7 @@ app.get("/formdata",(req,res)=>{
        studentList:savedUsers
    })
 })
-
+app.get("/form",formControl)
 app.listen(port,()=>{
     console.log(`App listening succesfully on ${port}`)
 })
